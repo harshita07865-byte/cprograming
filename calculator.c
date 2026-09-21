8bba2dff3c20f0babf2744001a5aeb7253f5b881
harshita07865-byte <harshita07865@gmail.com>
2026-09-21 23:18:37 +0530

    ADDED calculator

---
+++ calculator.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+int main(){
+    int a,b;
+    printf("Enter the two numbers:");
+    scanf("%d %d",&a,&b);
+    printf("Addition=%d\n",a+b);
+    printf("Subtraction=%d\n",a-b);
+    printf("Multiplication=%d\n",a*b);
+    printf("Modulus=%d\n",a%b);
+    printf("Diviso=%d\n3",a/b);
+    return 0;
+}
