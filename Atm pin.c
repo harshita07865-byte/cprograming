6501994bd6682c24bdd611983f877840b9733fd6
harshita07865-byte <harshita07865@gmail.com>
2026-09-22 22:46:12 +0530

    Add new atm menu

--- Atm pin.c
+++ Atm pin.c
@@ -1,27 +1,38 @@
 #include <stdio.h>
 int main(){
-    int pin,amount;// Declare the variable using int keyword.
+    int pin,amount,choice;// Declare the variable using int keyword.
     float balance=5000;//Declare the variable using float keyword.
-    printf("Enter the pin:");//Takes the pin from the user.
-    scanf("%d",&pin);//Takes the input that user has enetered and and stores to pin varaible.
-    if(pin==1456){//if statement starts
+    printf("Enter the pin:\n");//Takes the pin from the user.
+    scanf("%d",&pin);//Takes the input that user has enetered and  stores to pin varaible.
+    printf("Enter choice:");//Takes the choice from the user.
+    scanf("%d",&choice);//takes input and stores to the choice variable.
+    if(pin==1456){//if statement starts   
+        if(choice==1){//if condition starts here.
         printf("Enter withdrawl amount:");//Takes withdrawl amount from user.
         scanf("%d",&amount);//Takes input and stores it to amount variable
         if(amount<=balance){// Checks the condition and moves further then.
             balance = balance-amount;// Deduct balance.
-            printf("Your withdrawl amount is successful");
+            printf("Your withdrawl amount is successful\n");
             printf("Remaining balance=%f",balance);// New balance in account.
         }
         else{
-            printf("Insufficient amount");// Condition is not matched.
+            printf("Insufficient amount");// invalid amount.
         }
     }
-    else if(pin==1456){// condition starts from here
+    if(choice==2){
+    if (balance>0)
+ { // if condition starts from here
         printf("Enter deposit amount:");// Takes the amount from the user.
         scanf("%d",&amount);//Takes input and stores to the amount variable.
-        balance = balance + amount;
-        printf("Your deposit amount successful");
-        printf("New balance=%f",balance);// New balance after depositing.
+        balance = balance + amount;// deposit amount.
+        printf("Your deposit amount successful.\n");
+        printf("New balance=%f",balance);// New balance after depositing.     
+}
     }
-    return 0;// End of the program.
+    else{
+        printf("Invalid pin");// Invalid pin
+    }
+
+}   
+ return 0;// End of the program.
 }
---
+++ Recharge plan.c
@@ -0,0 +1,6 @@
+#include <stdio.h>
+int main(){
+    int choice , confirm;
+    printf("--Mobile plan--");
+    printf("1. 199plan\n")
+}
