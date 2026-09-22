f8d63f83fff082c10ea31a834df388df8e739cb6
harshita07865-byte <harshita07865@gmail.com>
2026-09-22 23:51:59 +0530

    Add Recharge menu

--- Recharge plan.c
+++ Recharge plan.c
@@ -1,19 +1,19 @@
 #include <stdio.h>
 int main(){
-    int choice , confirm;
-    printf("--Mobile plan--");
+    int choice , confirm;// Declared variable
+    printf("--Mobile plan--");// Menu displayed.
     printf("1. 199 plan\n");
     printf("2. 299 plan\n");
     printf("3. 399 plan\n");
-    printf("Enter the choice:");
-    scanf("%d",&choice);
-    switch (choice)
+    printf("Enter the choice:");// Takes choice from the user.
+    scanf("%d",&choice);// Takes input and stores to the variable.
+    switch (choice)// switch starts.
     {
     case 1:printf("You have selected 199 plan\n");
     printf("1.Yes\n");
     printf("2.No\n");
-    printf("Enter confirmation:");
-    scanf("%d",&confirm);
+    printf("Enter confirmation:");// Takes confirmation from the user.
+    scanf("%d",&confirm);// Takes input and store to the variable.
     if(confirm==1){
         printf("Your recharge plan for 199 is successful");
     }   else{
@@ -23,8 +23,8 @@ int main(){
     case 2:printf("You have selected 299 plan.\n");
     printf("1.Yes\n");
     printf("2.No\n");
-    printf("Enter confirmation:");
-    scanf("%d",&confirm);
+    printf("Enter confirmation:");// Takes confirmation from the user.
+    scanf("%d",&confirm);// Takes input and store to the variable.
     if(confirm==1){
         printf("Your recharge plan for 299 is successful");
     }   
@@ -34,8 +34,8 @@ int main(){
  case 3: printf("You have selected 399 plan");
  printf("1.Yes\n");
     printf("2.No\n");
-    printf("Enter confirmation:");
-    scanf("%d",&confirm);
+    printf("Enter confirmation:");// Takes confirmation from the user.
+    scanf("%d",&confirm);// Takes input and store to the variable.
     if(confirm==1){
         printf("Your recharge plan for 399 is successful");
     
@@ -44,6 +44,6 @@ int main(){
     }   break;
     default: printf("Invalid choice");
     
-    }
+    } // Ends of the program.
     return 0;
 }
