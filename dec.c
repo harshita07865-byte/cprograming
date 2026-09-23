9b199c38d29fe4f7bddcf223239b6664a719e0fe
harshita07865-byte <harshita07865@gmail.com>
2026-09-23 23:09:21 +0530

    Add dec code

---
+++ dec.c
@@ -0,0 +1,8 @@
+#include<stdio.h>
+int main(){// main function is the starting point of the execution of the c program
+    int a=10;// Declares avariable and initialize the value
+    int b=--a;// Decrement opreator is used.
+    printf("a=%d\n",a);// first decreases the value.
+    printf("b=%d\n",b);// then assigns to the b.
+    return 0;//End of the program.
+}
