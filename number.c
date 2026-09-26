9bf6946b5832bc8c096cf48d2eef4ef813b8825b
harshita07865-byte <harshita07865@gmail.com>
2026-09-26 15:50:34 +0530

    Add while lopp program to print 1 to 10

---
+++ number.c
@@ -0,0 +1,10 @@
+#include <stdio.h>// Header file for input output.
+int main(){// main is the function where the execution of the program starts.
+    int n=1;// Start counting from one.
+while (n<=10)// Repeat unitl the given condition becomes false.
+{
+    printf("%d\n",n);// prints the value of n.
+    n++;// increases the value by 1.
+}
+return 0;// End of the program.
+}
